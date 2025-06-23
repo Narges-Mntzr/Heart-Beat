21698c6d4c5179fd0fab24c7da07964c0e870066
nargesMntzr <narges.montazeri81@gmail.com>
2025-06-23 20:29:00 +0330

    Fix transation.

--- server/myproject/heartbeat/services.py
+++ server/myproject/heartbeat/services.py
@@ -13,18 +13,23 @@ def get_time_ago_str(dt):
         time_ago_str = _("Just now")
     elif seconds < 3600:
         minutes = int(seconds // 60)
-        time_ago_str = _("{} mins ago".format(minutes))
+        time_ago_str = _("{minutes} mins ago").format(minutes=en_to_fa_digits(minutes))
     elif seconds < 86400:
         hours = int(seconds // 3600)
-        time_ago_str = _(f"{hours} hours ago")
+        time_ago_str = _("{hours} hours ago").format(hours=en_to_fa_digits(hours))
     else:
         days = int(seconds // 86400)
-        time_ago_str = _(f"{days} days ago")
+        time_ago_str = _("{days} days ago").format(days=en_to_fa_digits(days))
 
     return time_ago_str
+
+def en_to_fa_digits(text):
+    en_to_fa = str.maketrans("0123456789", "۰۱۲۳۴۵۶۷۸۹")
+    return str(text).translate(en_to_fa)
 
 
 def to_jalali(datetime_obj):
     if datetime_obj is None:
         return "-"
-    return jdatetime.datetime.fromgregorian(datetime=datetime_obj).strftime('%Y/%m/%d')
+    jalali_str = jdatetime.datetime.fromgregorian(datetime=datetime_obj).strftime('%Y/%m/%d')
+    return en_to_fa_digits(jalali_str)
